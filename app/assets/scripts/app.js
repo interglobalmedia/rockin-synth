9369e545d91c0e83d94a495364f3bf669d92613c
interglobalmedia <interglobalmedia@gmail.com>
2016-12-13 21:40:35 -0500

    Update 2016-12-14T02:40:33.403Z

--- app/assets/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-import { playSound, removeTransition } from './modules/pianokeyboard';
-
-const keys = document.querySelectorAll('kbd');
-
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-window.addEventListener('keydown', playSound);
